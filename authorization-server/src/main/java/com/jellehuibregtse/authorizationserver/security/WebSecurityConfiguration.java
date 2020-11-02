03afcc98b077ba1c6985167040489ad5c9c506bf
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-11-02 21:37:42 +0100

    Remove Configuration annotation from WebSecurityConfiguration

--- authorization-server/src/main/java/com/jellehuibregtse/authorizationserver/security/WebSecurityConfiguration.java
+++ authorization-server/src/main/java/com/jellehuibregtse/authorizationserver/security/WebSecurityConfiguration.java
@@ -1,7 +1,6 @@
 package com.jellehuibregtse.authorizationserver.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  *
  * @author Jelle Huibregtse
  */
-@Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
