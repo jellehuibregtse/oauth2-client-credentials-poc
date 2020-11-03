e8dbaa5da748eda4c1a95f5bc00e23d3c6514139
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-11-03 11:36:40 +0100

    Add EnableEurekaServer annotation to discovery

--- discovery-server/src/main/java/com/jellehuibregtse/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/jellehuibregtse/discoveryserver/DiscoveryServerApplication.java
@@ -2,12 +2,18 @@ package com.jellehuibregtse.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * The entry point for the eureka server.
+ *
+ * @author Jelle Huibregtse
+ */
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
 }
